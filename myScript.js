3d54d9bb8625d8c3284ff9a65acb95e4fa0ae901
GianCastro11 <55288135+GianCastro11@users.noreply.github.com>
2020-01-15 11:56:25 -0500

    Update myScript.js

--- myScript.js
+++ myScript.js
@@ -1,1 +1,18 @@
+for(i = 0;i < 30; i++){
+  document.write ("Loop number"+ i +"<br />");
+}
 
+var beginning = 20;
+var limit = -20;
+
+/*while(beginning < limit)
+  {
+    document.write("Current Count: " +beginning )
+    beginnng++;
+  } */
+  
+  while(beginning > limit)
+    {
+      document.write(" Current Count: " + beginning + "<br />")
+      beginning = beginning -2;
+    }
